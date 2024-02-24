7539ae35b50325ce2edd4e35000a5a8be57f2cfc
Ravi-teach <154532347+Ravi-teach@users.noreply.github.com>
2024-02-24 10:27:55 +0000

    change

---
+++ javaScript/first.js
@@ -0,0 +1,7 @@
+// console.log("hello");
+// console.log(3-5)
+const Name ="Ravi Shanker"
+let F_Name ="Kasturchanad" 
+surname ="Suthar" 
+const number = "03020301248"
+ console.table([Name,F_Name,number,surname])
