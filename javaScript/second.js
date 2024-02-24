bcef8990dcc7f98d8ba32b01c73d8e4afb699e2f
Ravi-teach <154532347+Ravi-teach@users.noreply.github.com>
2024-02-24 10:45:14 +0000

    datatype

---
+++ javaScript/second.js
@@ -0,0 +1,8 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+console.log(typeof score);
+let valueInnumber = Number (score) // this sentex is use for boolen change into number
+console.log(typeof valueInnumber); // valueInnumber change  boolen into Number 
