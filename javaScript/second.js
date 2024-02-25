8a7a84312f8639ee68656abea54e188d3e8dbd56
Ravi-teach <154532347+Ravi-teach@users.noreply.github.com>
2024-02-25 14:49:28 +0000

    datatype

--- javaScript/second.js
+++ javaScript/second.js
@@ -1,8 +1,38 @@
-let score = true
+// Primitive and Non-primitive data-types in JavaScript
+// Primitive
+// there are seven type of pirmitive datatype
+// String,Number,null,Symbol,undefined,Boolean,BigInt
 
-console.log(typeof score);
-console.log(typeof (score));
+// +++++++++++Number++++++++++
 
-console.log(typeof score);
-let valueInnumber = Number (score) // this sentex is use for boolen change into number
-console.log(typeof valueInnumber); // valueInnumber change  boolen into Number 
+let x = 250;
+let y = 40.5;
+console.log("Value of x=" + x);
+console.log("Value of y=" + y);
+
+// ++++++++++String++++++++++
+  
+let str = 'Hello All';
+let str1 = "Welcome to my new house";
+console.log("Value of str=" + str);
+console.log("Value of str1=" + str1);
+
+
+// +++++++undefined+++++++++
+// console.log("Value of x=" + x);
+// it means value of x is undefined
+
+
+// null
+// let x = null;
+// console.log("Value of x=" + x);
+
+
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// console.log(typeof score);
+// let valueInnumber = Number (score) // this sentex is use for boolen change into number
+// console.log(typeof valueInnumber); // valueInnumber change  boolen into Number 
